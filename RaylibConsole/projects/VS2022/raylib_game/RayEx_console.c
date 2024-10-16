c2d9abca048aff43fc74761159178ccf9d1097a7
PI-Miners <42210633+PI-Miners@users.noreply.github.com>
2024-10-16 16:09:14 +0100

    RayEx_Console Initial Commit

---
+++ RaylibConsole/projects/VS2022/raylib_game/RayEx_console.c
@@ -0,0 +1,9 @@
+#include "raylib.h"
+
+
+bool rayEx_ConsoleOpen = false;
+
+
+bool RayEx_IsConsoleOpen();
+bool RayEx_IsConsoleSelected();
+void RayEx_ChangeConsoleState();
---
+++ RaylibConsole/projects/VS2022/raylib_game/RayEx_console.h
@@ -0,0 +1,8 @@
+#ifndef RAYEX_CONSOLE_H
+#define RAYEX_CONSOLE_H
+
+bool RayEx_IsConsoleOpen();
+bool RayEx_IsConsoleSelected();
+void RayEx_ChangeConsoleState();
+
+#endif // !RAYEX_CONSOLE_H
